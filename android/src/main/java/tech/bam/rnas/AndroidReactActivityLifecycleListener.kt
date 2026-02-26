7b198edfd0b48a20d2bb3a6159c2d85447081847
leenoyu <31582700+leenoyu@users.noreply.github.com>
2026-02-26 17:24:25 +0700

    Update AndroidReactActivityLifecycleListener.kt

--- android/src/main/java/tech/bam/rnas/AndroidReactActivityLifecycleListener.kt
+++ android/src/main/java/tech/bam/rnas/AndroidReactActivityLifecycleListener.kt
@@ -6,7 +6,6 @@ import android.view.WindowManager
 import android.os.Bundle
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
 
-import com.facebook.react.modules.network.OkHttpClientProvider;
 
 class AndroidReactActivityLifecycleListener : ReactActivityLifecycleListener {
   override fun onResume(activity: Activity) {
@@ -27,7 +26,6 @@ class AndroidReactActivityLifecycleListener : ReactActivityLifecycleListener {
 
   override fun onCreate(activity: Activity, savedInstanceState: Bundle?) {
     super.onCreate(activity, savedInstanceState)
-    OkHttpClientProvider.setOkHttpClientFactory(SSLPinning())
 
     if(BuildConfig.RNAS_PREVENT_RECENT_SCREENSHOTS && Build.VERSION.SDK_INT >= 33) {
       activity.setRecentsScreenshotEnabled(false)
