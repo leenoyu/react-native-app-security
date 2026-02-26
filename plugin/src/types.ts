075d33f2efdc70f9f6c68ce8fa13dfa1e15e8e44
leenoyu <31582700+leenoyu@users.noreply.github.com>
2026-02-26 17:22:39 +0700

    Update types.ts

--- plugin/src/types.ts
+++ plugin/src/types.ts
@@ -1,12 +1,5 @@
 export type RNASConfig = {
-  sslPinning?: {
-    [hostName: string]: string[];
-  };
   preventRecentScreenshots?: {
-    ios?: { enabled: boolean };
     android?: { enabled: boolean };
-  };
-  disableCache?: {
-    ios?: { enabled: boolean };
   };
 };
