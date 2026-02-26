4cceda9eedab0b071e5987dbc60ef1b1690a4692
leenoyu <31582700+leenoyu@users.noreply.github.com>
2026-02-26 17:23:01 +0700

    Update index.ts

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,14 +1,10 @@
 import { ConfigPlugin } from "@expo/config-plugins";
 import { RNASConfig } from "./types";
-import withDisableCache from "./withDisableCache";
 import withpreventRecentScreenshots from "./withPreventRecentScreenshots";
-import withSSLPinning from "./withSSLPinning";
 
 const withRNAS: ConfigPlugin<RNASConfig> = (config, props) => {
-  config = withSSLPinning(config, props.sslPinning);
 
   config = withpreventRecentScreenshots(config, props.preventRecentScreenshots);
-  config = withDisableCache(config, props.disableCache);
 
   return config;
 };
